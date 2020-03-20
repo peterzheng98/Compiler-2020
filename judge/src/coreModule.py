8e6187aa09fd3dfd6c60efffb95eb556d90e4b1e
Peter Zheng <peterzheng98@sjtu.edu.cn>
2020-03-20 08:54:58 +0800

    add(local-network adapter)

--- judge/src/coreModule.py
+++ judge/src/coreModule.py
@@ -120,3 +120,77 @@ def build_compiler(config_dict: dict):
         log = fetchGitCommit(userCompilerPath, hashResultRemote[1])
         return 'Success', hashResultRemote[1], base64.b64encode(log.encode()).decode(), base64.b64encode(
             'Recently built.'.encode()).decode(), imageName
+
+
+def build_compiler_local_adapter(config_dict: dict):
+    assert 'uuid' in config_dict.keys()
+    assert 'repo' in config_dict.keys()
+    userCompilerPath = Config_Dict['compilerPath'] + '/' + config_dict['uuid']
+    git_build_stdout = ''
+    git_build_stderr = ''
+    genLog('[coreModule.py] Generating repo with path: {}'.format(userCompilerPath))
+    if not os.path.exists(userCompilerPath):
+        os.makedirs(userCompilerPath)
+        try:
+            genLog('[coreModule.py] Start cloning object.')
+            process = subprocess.Popen(['git', 'clone', config_dict['repo'], '.'], cwd=userCompilerPath,
+                                       stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            process.wait(Config_Dict['GitTimeout'])
+            git_build_stdout = process.stdout.readlines()
+            git_build_stdout = ''.join([i.decode() for i in git_build_stdout])
+            git_build_stderr = process.stderr.readlines()
+            git_build_stderr = ''.join([i.decode() for i in git_build_stderr])
+        except subprocess.TimeoutExpired as identifier:
+            git_build_stderr = 'Git Timeout'
+            genLog('[coreModule.py] Git Timeout')
+            process.terminate()
+            return 'Fail', 'N/A', base64.b64encode('N/A'.encode()).decode(), base64.b64encode(
+                git_build_stderr.encode()).decode(), ''
+        except Exception as identifier:
+            git_build_stderr = 'GitRuntime Error: {}'.format(identifier)
+            genLog('[coreModule.py] {}'.format(git_build_stderr))
+            return 'Fail', 'N/A', base64.b64encode('N/A'.encode()).decode(), base64.b64encode(
+                git_build_stderr.encode()).decode(), ''
+    # Check the hash value
+    # 1. get local hash
+    hashResultLocal = getGitHash(userCompilerPath)
+    # 2. get remote hash
+    hashResultRemote = getGitHash(config_dict['repo'])
+    hashMatched = (hashResultLocal[0] == 1 and hashResultRemote[0] == 1 and hashResultLocal[1] ==
+                   hashResultRemote[1])
+    genLog('(build_compiler)Judging:local:%s, remote:%s, matched:%s' % (hashResultLocal, hashResultRemote, hashMatched))
+    # if not matched -> save a duplicated copy of the last version
+    # not matched: update the repo
+    if not hashMatched:
+        updateRepo(userCompilerPath, hashResultLocal, config_dict['repo'], config_dict['uuid'])
+    gitCommitLog = fetchGitCommit(userCompilerPath, hashResultRemote[1])
+
+    process = None
+    try:
+        process = subprocess.Popen(["bash", 'build.bash'], cwd=userCompilerPath,
+                                   stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=False)
+        process.wait(90)
+        stdout_result = process.stdout.readlines()
+        stderr_result = process.stderr.readlines()
+        stdout_result_str = ''.join([i.decode() for i in stdout_result])
+        stderr_result_str = ''.join([i.decode() for i in stderr_result])
+        log = '==git stdout==\n{}\n==git stderr==\n{}\n==stdout==\n{}\n==stderr==\n{}'.format(
+            git_build_stdout, git_build_stderr, stdout_result_str, stderr_result_str)
+
+        if process.returncode == 0:
+            return 'Success', hashResultRemote[1], base64.b64encode(gitCommitLog.encode()).decode(), base64.b64encode(
+                log.encode()).decode(), 'local'
+        else:
+            return 'Fail', hashResultRemote[1], base64.b64encode(gitCommitLog.encode()).decode(), base64.b64encode(
+                log.encode()).decode(), 'local'
+    except subprocess.TimeoutExpired as identifier:
+        try:
+            process.kill()
+        except Exception as identifier:
+            pass
+        pass
+        return 'Timeout', hashResultRemote[1], base64.b64encode(gitCommitLog.encode()).decode(), base64.b64encode(
+            'build time out'.encode()).decode(), 'local'
+    except Exception as identifier:
+        return 'Runtime Error', hashResultRemote[1], base64.b64encode(gitCommitLog.encode()).decode(), base64.b64encode(
+            'build runtime error'.encode()).decode(), 'local'
